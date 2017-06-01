586a80957de3f5cf569d63ec3392efc172951530
蚊香 <1542997629@qq.com>
2017-06-01 21:11:37 +0800

    save

--- 4.Median_of_Two_Sorted_Arrays.go
+++ 4.Median_of_Two_Sorted_Arrays.go
@@ -14,3 +14,12 @@ package leetcode
 // nums2 = [3, 4]
 
 // The median is (2 + 3)/2 = 2.5
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	result := float64(0)
+	nums3 := make([]int, len(nums1)+len(nums2))
+	i := 0
+	j := 0
+	for i < len(nums1) || j < len(nums2) {
+
+	}
+}
