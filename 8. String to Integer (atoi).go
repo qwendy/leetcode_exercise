acf30d16e8f2137ee4af204d7f514ce494586831
qianwenxiang <1542997629@qq.com>
2017-08-14 22:24:09 +0800

    finished Problem8

--- 8. String to Integer (atoi).go
+++ 8. String to Integer (atoi).go
@@ -8,3 +8,57 @@ package leetcode
 
 // Update (2015-02-10):
 // The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
+func myAtoi(str string) int {
+	result := int64(0)
+	times := int64(1)
+	s := 0
+	out := false
+
+	for index := len(str) - 1; index >= 0; index-- {
+		if str[index] < '0' || str[index] > '9' {
+			if str[index] == '-' {
+				s++
+				result = -result
+			}
+			if str[index] == '+' {
+				s++
+			}
+			if s == 2 {
+				return 0
+			}
+			if s == 1 {
+				continue
+			}
+			if str[index] == ' ' && (index == 0 || str[index-1] == ' ') {
+				continue
+			}
+			times = 1
+			result = 0
+			continue
+		}
+		if !out {
+			s := int64(str[index] - '0')
+			result = result + times*(s)
+			times = times * 10
+		}
+		if result > 2147483647 || result < -2147483648 {
+			out = true
+		} else {
+			out = false
+		}
+	}
+	if result > 2147483647 || result < -2147483648 {
+		out = true
+	} else {
+		out = false
+	}
+	if out {
+		if result > 0 {
+			result = 2147483647
+		} else {
+			result = -2147483648
+		}
+	}
+
+	return int(result)
+}
---
+++ 8. String to Integer (atoi)_test.go
@@ -0,0 +1,77 @@
+package leetcode
+
+import "testing"
+
+func Test_myAtoi(t *testing.T) {
+	type args struct {
+		str string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{
+				str: "123",
+			},
+			want: 123,
+		},
+		{
+			args: args{
+				str: "-123",
+			},
+			want: -123,
+		},
+		{
+			args: args{
+				str: "+-2",
+			},
+			want: 0,
+		},
+		{
+			args: args{
+				str: "     +004500",
+			},
+			want: 4500,
+		},
+		{
+			args: args{
+				str: "  -0012a42",
+			},
+			want: -12,
+		},
+		{
+			args: args{
+				str: "123  456",
+			},
+			want: 123,
+		},
+		{
+			args: args{
+				str: "   010",
+			},
+			want: 10,
+		},
+		{
+			args: args{
+				str: "18446744073709551617",
+			},
+			want: 2147483647,
+		},
+		{
+			args: args{
+				str: "  +b12102370352",
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := myAtoi(tt.args.str); got != tt.want {
+				t.Errorf("myAtoi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
