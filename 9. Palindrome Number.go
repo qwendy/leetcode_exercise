bc7a512499ed5723b353fd70685dba9bf28f3c2b
蚊香 <1542997629@qq.com>
2017-09-04 18:44:47 +0800

    9 finished

---
+++ 9. Palindrome Number.go
@@ -0,0 +1,12 @@
+package leetcode
+
+// Determine whether an integer is a palindrome. Do this without extra space.
+
+func isPalindrome(x int) bool {
+	y, t := 0, x
+	for t > 0 {
+		y = y*10 + t%10
+		t = t / 10
+	}
+	return x == y
+}
