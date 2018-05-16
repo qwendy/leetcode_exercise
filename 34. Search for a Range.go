702ad59bf1eb34b13bd75e53317f2a03f991dfe2
蚊香 <1542997629@qq.com>
2018-05-16 16:16:09 +0800

    problem 34 accepted

--- 34. Search for a Range.go
+++ 34. Search for a Range.go
@@ -4,12 +4,36 @@ package leetcode
 func searchRange(nums []int, target int) []int {
 	n := len(nums)
 	left := 0
-	right := 0
-	for left < right {
+	right := n - 1
+	start := -1
+	end := -1
+	for left <= right {
 		mid := (right + left) / 2
 		if target > nums[mid] {
-
+			left = mid + 1
+		}
+		if target < nums[mid] {
+			right = mid - 1
+		}
+		if target == nums[mid] {
+			start = mid
+			end = mid
+			for i := mid - 1; i >= 0; i-- {
+				if nums[i] == target {
+					start--
+				} else {
+					break
+				}
+			}
+			for i := mid + 1; i < n; i++ {
+				if nums[i] == target {
+					end++
+				} else {
+					break
+				}
+			}
+			break
 		}
 	}
-	return []int{-1, -1}
+	return []int{start, end}
 }
