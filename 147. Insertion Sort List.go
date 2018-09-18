519933cd86ed8b6d560e0e685bd19225162cc5ae
蚊香 <1542997629@qq.com>
2018-09-18 17:20:13 +0800

    to be continued

--- 147. Insertion Sort List.go
+++ 147. Insertion Sort List.go
@@ -3,29 +3,60 @@
 package leetcode
 
 func insertionSortList(head *ListNode) *ListNode {
+	headPre := &ListNode{Next: head}
+
+	cur := head
+	for cur != nil && cur.Next != nil {
+		p := cur.Next
+		if cur.Val <= p.Val {
+			cur = p
+			continue
+		}
+		cur.Next = p.Next
+
+		pre := headPre
+		next := headPre.Next
+
+		for next.Val < p.Val {
+			pre = next
+			next = next.Next
+		}
+		pre.Next = p
+		p.Next = next
+	}
+	return headPre.Next
+}
+
+func insertionSortListBad(head *ListNode) *ListNode {
 	if head == nil || head.Next == nil {
 		return head
 	}
+	last := head
 	p := head.Next
-	for ; p != nil; p = p.Next {
-		t := p.Val
-
-		for iter := head; iter != p; iter = iter.Next {
-			if t < iter.Val {
-				v := iter.Val
-				iter.Val = t
-				iter = iter.Next
-
-				for {
-					v, iter.Val = iter.Val, v
-					if iter == p {
-						break
-					}
-					iter = iter.Next
-				}
+	for p != nil {
+		if head.Val > p.Val {
+			last.Next = p.Next
+			p.Next = head
+			head = p
+			p = last.Next
+			continue
+		}
+		ok := false
+		for iter := head; iter.Next != nil && iter.Next != p; iter = iter.Next {
+			if iter.Next.Val > p.Val {
+				last.Next = p.Next
+				p.Next = iter.Next
+				iter.Next = p
+				p = last.Next
+				ok = true
 				break
 			}
 		}
+		if !ok {
+			last = p
+			p = p.Next
+		}
+
 	}
 	return head
 }
