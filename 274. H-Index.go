b5390f64c6eefd8af12d800d9e7c9b9834038521
蚊香 <1542997629@qq.com>
2018-09-21 18:00:25 +0800

    xxx

---
+++ 274. H-Index.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/h-index/description/
+package leetcode
+
+import (
+	"sort"
+)
+
+func hIndex(citations []int) int {
+	sort.Slice(citations, func(i, j int) bool {
+		return citations[i] > citations[j]
+	})
+	for i, v := range citations {
+		if i+1 > v {
+			return i
+		}
+	}
+	return len(citations)
+}
---
+++ 324. Wiggle Sort II.go
@@ -0,0 +1,6 @@
+// https://leetcode.com/problems/wiggle-sort-ii/description/
+package leetcode
+
+func wiggleSort(nums []int) {
+
+}
