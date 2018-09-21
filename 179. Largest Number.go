95a4f03234e6611506faece86d1e1498563e390f
蚊香 <1542997629@qq.com>
2018-09-21 16:39:57 +0800

    179. finiished

---
+++ 179. Largest Number.go
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/largest-number/description/
+package leetcode
+
+import (
+	"bytes"
+	"sort"
+	"strconv"
+)
+
+func largestNumber(nums []int) string {
+	var ss []string
+	for _, v := range nums {
+		ss = append(ss, strconv.Itoa(v))
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		k := 0
+		for ; k < len(ss[i]) && k < len(ss[j]); k++ {
+			if ss[i][k] != ss[j][k] {
+				return ss[i][k] > ss[j][k]
+			}
+		}
+		if k < len(ss[i]) {
+			for ; k < len(ss[i]); k++ {
+				if ss[i][k] != ss[i][0] {
+					return ss[i][k] > ss[i][0]
+				}
+			}
+			return ss[i][1] < ss[i][0]
+		}
+		if k < len(ss[j]) {
+			for ; k < len(ss[j]); k++ {
+				if ss[j][k] != ss[j][0] {
+					return !(ss[j][k] > ss[j][0])
+				}
+			}
+			return ss[j][1] > ss[j][0]
+		}
+		return false
+		// return ss[i]+ss[j] > ss[j]+ss[i]
+	})
+	var buffer bytes.Buffer
+	for i, v := range ss {
+		if i == 0 && v == "0" {
+			return "0"
+		}
+		buffer.WriteString(v)
+	}
+	return buffer.String()
+}
---
+++ 179. Largest Number_test.go
@@ -0,0 +1,47 @@
+package leetcode
+
+import "testing"
+
+func Test_largestNumber(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			args: args{
+				nums: []int{3, 30, 34, 5, 9},
+			},
+			want: "9534330",
+		},
+		{
+			args: args{
+				nums: []int{20, 1},
+			},
+			want: "201",
+		},
+		{
+			args: args{
+				nums: []int{824, 938, 1399, 5607, 6973, 5703, 9609, 4398, 8247},
+			},
+			want: "9609938824824769735703560743981399",
+		},
+		{
+			args: args{
+				nums: []int{121, 12},
+			},
+			want: "12121",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := largestNumber(tt.args.nums); got != tt.want {
+				t.Errorf("largestNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
