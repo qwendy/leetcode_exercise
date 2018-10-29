10594d3e89ec30134a1e740e8c8b3461ac6cac92
蚊香 <1542997629@qq.com>
2018-10-29 18:06:37 +0800

    xxx

--- practice/avl_tree.go
+++ practice/avl_tree.go
@@ -6,9 +6,42 @@ type avlTree struct {
 }
 
 func (avl *avlTree) height(x *node) int {
-	max := avl.height(x.left)
-	if max < avl.height(x.right) {
-		max = avl.height(x.right)
+	if x == nil {
+		return 0
 	}
-	return max + 1
+	return x.H
+}
+
+func (avl *avlTree) put(key, val interface{}) {
+	avl.root = avl.putNode(key, val, avl.root)
+}
+
+func (avl *avlTree) putNode(key, val interface{}, x *node) *node {
+	if x == nil {
+		return &node{key: key, val: val, H: 1}
+	}
+
+	r := Compare(key, x.key)
+	if r < -1 {
+		avl.putNode(key, val, x)
+	} else if r > 1 {
+		avl.putNode(key, val, x)
+	} else {
+		x.val = val
+	}
+	max := func(a, b int) int {
+		if a > b {
+			return a
+		}
+		return b
+	}
+	x.H = max(avl.height(x.left), avl.height(x.right)) + 1
+
+	balance := avl.height(x.left) - avl.height(x.right)
+
+	// TODO:  left rotate
+	if balance > 1 && Compare(key, x.left.key) < -1 {
+	}
+
+	return x
 }
