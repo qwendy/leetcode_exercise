bdfc4d0c65f4a9c265338a37b145c619b3bf5980
qianwenxiang <1542997629@qq.com>
2018-11-11 23:14:25 +0800

    finished Problem154

---
+++ 154. Find Minimum in Rotated Sorted Array II.go
@@ -0,0 +1,19 @@
+package leetcode
+
+func findMin_154(nums []int) int {
+	start, end := 0, len(nums)-1
+	for start < end {
+		if nums[start] < nums[end] {
+			return nums[start]
+		}
+		mid := (start + end) / 2
+		if nums[mid] > nums[end] {
+			start = mid + 1
+		} else if nums[mid] < nums[end] {
+			end = mid
+		} else {
+			end--
+		}
+	}
+	return nums[start]
+}
