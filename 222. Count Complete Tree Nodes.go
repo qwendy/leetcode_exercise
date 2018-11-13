570d5d7c173653718fec6696eb6d1410e48519d6
皆非 <1542997629@qq.com>
2018-11-13 17:27:58 +0800

    xxx

---
+++ 222. Count Complete Tree Nodes.go
@@ -0,0 +1,14 @@
+package practice
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	return countNodes(root.Left) + countNodes(root.Right) + 1
+}
