5a8808e9d1697c6e986c81afe3ca9d3f6ddb4e2d
qianwenxiang <1542997629@qq.com>
2018-11-13 21:08:59 +0800

    xxx

--- 174. Dungeon Game.go
+++ 174. Dungeon Game.go
@@ -1,11 +1,18 @@
 package practice
 
+import (
+	"math"
+)
+
 func calculateMinimumHP(dungeon [][]int) int {
 	m := len(dungeon)
 	n := len(dungeon[0])
 	hp := make([][]int, m+1)
 	for i := 0; i < m+1; i++ {
 		hp[i] = make([]int, n+1)
+		for j := 0; j < n+1; j++ {
+			hp[i][j] = math.MaxInt32
+		}
 	}
 	hp[m][n-1] = 1
 	hp[m-1][n] = 1
