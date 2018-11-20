8c01c47da7e6cfe662614e86514d2bc09d9afef3
皆非 <1542997629@qq.com>
2018-11-20 17:16:45 +0800

    xxx

--- 300. Longest Increasing Subsequence.go
+++ 300. Longest Increasing Subsequence.go
@@ -1,5 +1,22 @@
 package practice
 
 func lengthOfLIS(nums []int) int {
-
+	tail := make([]int, len(nums))
+	size := 0
+	for _, x := range nums {
+		left, right := 0, size
+		for left < right {
+			mid := left + (right-left)/2
+			if x > tail[mid] {
+				left = mid + 1
+			} else {
+				right = mid
+			}
+		}
+		if size == left {
+			size++
+		}
+		tail[left] = x
+	}
+	return size
 }
---
+++ 354. Russian Doll Envelopes.go
@@ -0,0 +1,5 @@
+package practice
+
+func maxEnvelopes(envelopes [][]int) int {
+
+}
