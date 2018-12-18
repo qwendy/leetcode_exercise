d39248aefd01e8f330c43d8eba5a7839e73e9259
qianwenxiang <1542997629@qq.com>
2018-12-19 01:51:04 +0800

    xxxx

--- 探索/初级算法/数组/1. 从排序数组中删除重复项_test.go
+++ 探索/初级算法/数组/1. 从排序数组中删除重复项_test.go
@@ -23,7 +23,7 @@ func Test_removeDuplicates(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := removeDuplicates(tt.args.nums); got != tt.want {
-				t.Errorf("removeDuplicates() = %v, want %v", got, tt.want, tt.args.nums)
+				t.Errorf("removeDuplicates() = %v, want %v %v", got, tt.want, tt.args.nums)
 			}
 		})
 	}
