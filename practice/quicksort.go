673a3653b6375d149ef2ec8167c101b82cbca03f
皆休 <1542997629@qq.com>
2019-07-16 14:06:29 +0800

    quickSort finished

--- practice/quicksort.go
+++ practice/quicksort.go
@@ -5,13 +5,27 @@ func quicksort(nums []int, low, high int) {
 		insertSort(nums, low, high)
 		return
 	}
-	p := quickPartition(nums, low, high)
-	quicksort(nums, low, p-1)
-	quicksort(nums, p+1, high)
+	lt, gt := quickPartition(nums, low, high)
+	quicksort(nums, low, lt-1)
+	quicksort(nums, gt+1, high)
 
 }
 
-func quickPartition(nums []int, low, high int) int {
-	i, lt, gt := low, low+1, high
-
+func quickPartition(nums []int, low, high int) (int, int) {
+	i, lt, gt := low+1, low, high
+	v := nums[low]
+	for i <= high && lt < gt {
+		switch {
+		case nums[i] < v:
+			nums[i], nums[lt] = nums[lt], nums[i]
+			lt++
+			i++
+		case nums[i] > v:
+			nums[i], nums[gt] = nums[gt], nums[i]
+			gt--
+		default:
+			i++
+		}
+	}
+	return lt, gt
 }
