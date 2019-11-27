f48dc1fb2048ffdcf3e03662a9b1c6f28a50f8e4
莫斩风 <1542997629@qq.com>
2019-11-28 01:14:15 +0800

    xxxx

--- 169.多数元素.go
+++ 169.多数元素.go
@@ -6,7 +6,20 @@
 package practice
 // @lc code=start
 func majorityElement(nums []int) int {
-
+	max := nums[0]
+	count := 1
+	for i:=1 ; i< len(nums); i++ {
+		if max != nums[i] {
+			if count ==1 {
+				max = nums[i]
+			}else {
+				count--
+			}
+		}else {
+			count++
+		}
+	}
+	return max 
 }
 // @lc code=end
 
---
+++ 172.阶乘后的零.go
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=172 lang=golang
+ *
+ * [172] 阶乘后的零
+ */
+
+// @lc code=start
+package practice
+func trailingZeroes(n int) int {
+	count := 0
+	tail := 1
+	for i:=2; i< n ; i++ {
+		return 
+	} 
+	return count
+}
+// @lc code=end
+
