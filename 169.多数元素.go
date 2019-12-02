710b35331a20338dead193703f3e363ee07f1dd8
qianwenxiang <1542997629@qq.com>
2019-12-03 01:09:54 +0800

    190 to be finished

---
+++ 169.多数元素.go
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=169 lang=golang
+ *
+ * [169] 多数元素
+ *
+ * https://leetcode-cn.com/problems/majority-element/description/
+ *
+ * algorithms
+ * Easy (60.58%)
+ * Likes:    373
+ * Dislikes: 0
+ * Total Accepted:    88.5K
+ * Total Submissions: 145.9K
+ * Testcase Example:  '[3,2,3]'
+ *
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ *
+ * 示例 2:
+ *
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ *
+ *
+ */
+
+// @lc code=start
+func majorityElement(nums []int) int {
+
+	return
+}
+// @lc code=end
+
