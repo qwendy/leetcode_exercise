b9b207f0467027b5d3d5baa39034dbfc711c121b
qianwenxiang <qianwenxiang0501>
2021-08-25 00:46:47 +0800

    P135

---
+++ go_2th/135.Candy_test.go
@@ -0,0 +1,36 @@
+package practise
+
+import "testing"
+
+func Test_candy(t *testing.T) {
+	type args struct {
+		ratings []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "",
+			args: args{
+				ratings: []int{1, 0, 2},
+			},
+			want: 5,
+		},
+		{
+			name: "",
+			args: args{
+				ratings: []int{1, 3, 2, 2, 1},
+			},
+			want: 7,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := candy(tt.args.ratings); got != tt.want {
+				t.Errorf("candy() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
