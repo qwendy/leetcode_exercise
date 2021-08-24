f83b15bbf96f24cfb92764fa5e678accb9c61e40
qianwenxiang <qianwenxiang0501>
2021-08-25 01:59:33 +0800

    P435 error

---
+++ go_2th/435.Non-overlapping Intervals.go
@@ -0,0 +1,77 @@
+package practise
+
+import "sort"
+
+//Non-overlapping Intervals
+////Given an array of intervals intervals where intervals[i] = [starti, endi], ret
+//urn the minimum number of intervals you need to remove to make the rest of the i
+//ntervals non-overlapping.
+//
+//
+// Example 1:
+//
+//
+//Input: intervals = [[1,2],[2,3],[3,4],[1,3]]
+//Output: 1
+//Explanation: [1,3] can be removed and the rest of the intervals are non-overla
+//pping.
+//
+//
+// Example 2:
+//
+//
+//Input: intervals = [[1,2],[1,2],[1,2]]
+//Output: 2
+//Explanation: You need to remove two [1,2] to make the rest of the intervals no
+//n-overlapping.
+//
+//
+// Example 3:
+//
+//
+//Input: intervals = [[1,2],[2,3]]
+//Output: 0
+//Explanation: You don't need to remove any of the intervals since they're alrea
+//dy non-overlapping.
+//
+//
+//
+// Constraints:
+//
+//
+// 1 <= intervals.length <= 105
+// intervals[i].length == 2
+// -5 * 104 <= starti < endi <= 5 * 104
+//
+// Related Topics Array Dynamic Programming Greedy Sorting
+// 👍 2525 👎 73
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func eraseOverlapIntervals(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] < intervals[j][1]
+		}
+		return intervals[i][0] < intervals[j][0]
+	})
+	count := 0
+	last := intervals[0]
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] < last[1] {
+			count++
+			last = intervals[i-1]
+			if intervals[i][1] < intervals[i-1][1] {
+				last = intervals[i]
+			}
+		} else {
+			last = intervals[i]
+		}
+
+	}
+	return count
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
